acd8d4fa03251e07f97d0d7b947e8d1c547e5b36
Camiel Schilham <cfschilham@protonmail.com>
2024-03-30 21:01:31 +0100

    change back to author class instaed of author id in book class

--- src/models/book.ts
+++ src/models/book.ts
@@ -1,15 +1,16 @@
 import type { Database } from "sqlite";
+import { Author } from "./author.ts";
 
 export class Book {
   public isbn: number;
-  public authorId: number;
+  public author: Author;
   public title: string;
   public coverImageUrl: string;
   public description: string;
 
-  constructor(isbn: number, authorId: number, title: string, coverImageUrl: string, description: string) {
+  constructor(isbn: number, author: Author, title: string, coverImageUrl: string, description: string) {
     this.isbn = isbn;
-    this.authorId = authorId;
+    this.author = author;
     this.title = title;
     this.coverImageUrl = coverImageUrl;
     this.description = description;
@@ -21,16 +22,16 @@ export class Book {
       return null;
     }
 
-    return new Book(book.isbn, book.author_id, book.title, book.cover_image_url, book.description);
+    return new Book(book.isbn, new Author("", "", "", book.author_id), book.title, book.cover_image_url, book.description);
   }
 
-  static async getPageWithAuthorNames(index: number, db: Database): Promise<[Book, string][]> {
-    const books: [Book, string][] = [];
+  static async getPageWithAuthorNames(index: number, db: Database): Promise<Book[]> {
+    const books: Book[] = [];
     const query: string = "SELECT books.*, authors.first_name, authors.last_name FROM books LEFT JOIN authors ON books.author_id = authors.author_id ORDER BY isbn LIMIT 10 OFFSET ?";
 
     await db.each(query, index * 10, (err, row) => {
       if (err) { return; }
-      books.push([new Book(row.isbn, row.author_id, row.title, row.cover_image_url, row.description), row.first_name + row.last_name]);
+      books.push(new Book(row.isbn, new Author(row.first_name, row.last_name, "", row.author_id), row.title, row.cover_image_url, row.description));
     });
 
     return books;
@@ -39,7 +40,7 @@ export class Book {
   static async create(book: Book, db: Database): Promise<boolean> {
     const result = await db.run(
       "INSERT INTO books VALUES (?, ?, ?, ?, ?)",
-      book.isbn, book.authorId, book.title, book.coverImageUrl, book.description,
+      book.isbn, book.author.id, book.title, book.coverImageUrl, book.description,
     );
 
     return result.lastID != null;
